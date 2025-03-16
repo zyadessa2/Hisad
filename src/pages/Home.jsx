f1ad5147b483921fedce62b6766c0f17591c173b
ziad essa <helmyzez@gmail.com>
2025-03-16 15:39:41 +0200

    Add yocto-queue to fix build error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
     <>
 
 <Helmet>
+<meta name="google-site-verification" content="a65RsQZA0eonsEZFcuAzrUrXrBZdo1BygAHiGlHBrR8" />
         <title>حصاد للتقنية الزراعية المتطورة | حلول زراعية مبتكرة</title>
         <meta
           name="description"
